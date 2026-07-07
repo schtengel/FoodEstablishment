1be46aadc4d6aa7aeea60813ac82f8d39b3c293f
schtengel <katzilipsiz@gmail.com>
2026-07-07 22:06:39 +0500

    fix: исправлен тип данных для Id

--- FoodEstablishment.Api/DTOs/StorageZoneResponse.cs
+++ FoodEstablishment.Api/DTOs/StorageZoneResponse.cs
@@ -2,7 +2,7 @@ namespace FoodEstablishment.Api.DTOs;
 
 public class StorageZoneResponse
 {
-    public string Id { get; set; } = string.Empty;
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public decimal RecommendedTemperature { get; set; }
 }
