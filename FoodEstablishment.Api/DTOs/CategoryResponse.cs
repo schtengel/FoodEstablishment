99fb79fbb1e6279cc6c3313447c79e32eafda3cc
schtengel <katzilipsiz@gmail.com>
2026-07-07 16:30:38 +0500

    feat: добавлена базовая модель, модель category и сопутствующие ей DTOs

---
+++ FoodEstablishment.Api/DTOs/CategoryResponse.cs
@@ -0,0 +1,8 @@
+namespace FoodEstablishment.Api.DTOs;
+
+public class CategoryResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
