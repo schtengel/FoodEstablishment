759fb20813b85146dfe1d0b30b58248e185b48e8
schtengel <katzilipsiz@gmail.com>
2026-07-07 19:50:25 +0500

    feat: добавлена модель PaymentStatus.cs и ее DTO

---
+++ FoodEstablishment.Api/DTOs/PaymentStatusResponse.cs
@@ -0,0 +1,7 @@
+namespace FoodEstablishment.Api.DTOs;
+
+public class PaymentStatusResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
---
+++ FoodEstablishment.Api/Entities/PaymentStatus.cs
@@ -0,0 +1,6 @@
+namespace FoodEstablishment.Api.Entities;
+
+public class PaymentStatus : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+}
