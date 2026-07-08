f3fad22b98f5b9607bd7075c0cd1268bfb826706
schtengel <katzilipsiz@gmail.com>
2026-07-09 01:43:38 +0500

    refactor: replace annotations to fluent validation in storage zones

--- FoodEstablishment.Api/Program.cs
+++ FoodEstablishment.Api/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddSingleton<TokenService>();
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssemblyContaining<ProductCreateRequestValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<CategoryCreateRequestValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<StorageZoneCreateRequestValidator>();
 
 var jwtSecret = builder.Configuration["JwtSettings:Secret"] ?? throw new InvalidOperationException("JWT Secret is missing");
 builder.Services.AddAuthentication(options =>
